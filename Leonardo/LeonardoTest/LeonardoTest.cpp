f89d13e088401c1ce37ec2ab48154e29a30fc412
Elias Kramer <67228319+EliasKramer@users.noreply.github.com>
2023-03-17 08:42:27 +0100

    added ai project and test project

---
+++ Leonardo/LeonardoTest/LeonardoTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LeonardoTest
+{
+	TEST_CLASS(LeonardoTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
