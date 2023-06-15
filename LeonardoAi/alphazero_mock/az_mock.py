e70ca027090f87250f1e3668d4a6c0304df626b6
EliasKramer <kramer.elias@outlook.de>
2023-06-15 10:26:46 +0200

    will delete old approach

--- LeonardoAi/alphazero_mock/az_mock.py
+++ LeonardoAi/alphazero_mock/az_mock.py
@@ -1,11 +1,26 @@
-# https://towardsdatascience.com/alphazero-chess-how-it-works-what-sets-it-apart-and-what-it-can-tell-us-4ab3d2d08867
+# https://towardsdatascie   nce.com/alphazero-chess-how-it-works-what-sets-it-apart-and-what-it-can-tell-us-4ab3d2d08867
 
-def search(game, policy_nn, prediction_nn):
+# data structure for visited states
+# append and check if visited
+
+# data structure for Q, N, P
+# | input matrix | move -> float
+# | input matrix | move -> float
+# random access
+# map(matrix, map(matrix, float))
+
+
+def search(game, policy_network, prediction_nn):
     if gameOver(input_matrix): return outcome(input_matrix)
 
     if game.input_matrix() not in visited:
         visited.append(game.input_matrix())
-        P[game.input_matrix()][all_moves], evaluation = prediction_nn.predict(game.input_matrix())
+        output_matrix = policy_network.forward_prop(game.input_matrix())
+        for curr_move in legalMoves(game):
+            P[game.input_matrix()][curr_move] = output_matrix[curr_move]
+            N[game.input_matrix()][curr_move] = 1 # number of times the current move was explored - maybe 0?
+
+        evaluation = prediction_nn.forward_prop(game.input_matrix())
         return -evaluation
 
     max_u = -float("inf")
@@ -16,7 +31,11 @@ def search(game, policy_nn, prediction_nn):
         # P indicates how promising a move is
         # N is the number of times the current move was explored
         # u is the best move-exploration-evaluation-score
-        u = Q[game.input_matrix()][curr_move] + c * \
+        u = \
+            #exploitation - which move has the highest average evaluation
+            Q[game.input_matrix()][curr_move] + c * \
+            #exploration - which move has the highest promise
+            # - was evaluated very high, but not explored much
             P[game.input_matrix()][curr_move] * \
             sqrt(sum(N[game.input_matrix()])) \
             / (1 + N[game.input_matrix()][curr_move])
@@ -24,34 +43,35 @@ def search(game, policy_nn, prediction_nn):
         if u > max_u:
             max_u = u
             best_move = curr_move
-    curr_move = best_move
 
-    new_game = makeMove(game, curr_move)
+    new_game = makeMove(game, best_move)
     # we make the best move with e&e in mind and then search again
     # we do this recursively until we reach a leaf node
-    evaluation = search(new_game, policy_nn, prediction_nn)
+    evaluation = search(new_game, policy_network, prediction_nn)
 
-    Q[game.input_matrix()][curr_move] = \
+    Q[game.input_matrix()][best_move] = \
         # current number of moves * current average evaluation + evaluation of current move
-        # divided by current number of moves + 1
-        (N[game.input_matrix()][curr_move] * Q[game.input_matrix()][curr_move] + evaluation) / \
-        (N[input_matrix][curr_move] + 1)
-
-    #add 1 to the number of times the current move was explored
-    global N[input_matrix][curr_move]
-    N[input_matrix][curr_move] += 1
-    return -evaluation
+    # divided by current number of moves + 1
+    (N[game.input_matrix()][curr_move] * Q[game.input_matrix()][curr_move] + evaluation) / \
+    (N[input_matrix][curr_move] + 1)
 
 
-def policy(game, policy_network, prediction_network):
+# add 1 to the number of times the current move was explored
+N[input_matrix][curr_move] += 1
+return -evaluation
 
-    #number_of_simulations = 25000 on alphazero
+
+def policy(game,policy_network, prediction_network):
+    # number_of_simulations = 1600 on alphazero
     for i in range(number_of_simulations):
         search(game, policy_network, prediction_network)
-        #s = input_matrix
-        for curr_move in legal_moves(game)
-            output_matrix[curr_move] = N[game.input_matrix()][curr_move]
+
+    output_matrix;
+    for curr_move in legal_moves(game)
+        output_matrix[curr_move] = N[game.input_matrix()][curr_move]
+
     return output_matrix
+
 
 # ----------------------------------------------
 # TODO implement chess data space
@@ -65,7 +85,7 @@ def data(policy_network, prediction_network):
     for i in range(number_of_selfplay_games):
         # star game - duh
         game = startingPosition()
-        chess_data_space.new_game() # remember index, to add outcome later
+        chess_data_space.new_game()  # remember index, to add outcome later
         while True:
             # get move of policy network
             output_matrix = policy(game, policy_network, prediction_network)
@@ -94,15 +114,16 @@ def upgrade(
     new_policy_network.train(chess_data_space)
     new_win_prediction_model.train(chess_data_space)
 
+
 # ----------------------------------------------
 def finalnet():
     # neural network
     policy_nn;  # initialize
-    prediction_nn; # initialize
+    prediction_nn;  # initialize
     new_policy_nn;  # initialize
-    new_prediction_nn; # initialize
+    new_prediction_nn;  # initialize
 
-    # training
+    # training 25000 apparently
     for i in range(iterations):
         # learn on games
         upgrade(
